c2443d1983ce91c1801f50af4d7a16fbb2c85e2d
tenzin2017 <dolma@Owners-MacBook-Pro.local>
2017-08-15 12:40:03 -0400

    odds of sum coming up solved

---
+++ odds.rb
@@ -0,0 +1,25 @@
+die = [1, 2, 3, 4, 5, 6]
+sum_total = { 2 => 0,
+              3 => 0,
+              4 => 0,
+              5 => 0,
+              6 => 0,
+              7 => 0,
+              8 => 0,
+              9 => 0,
+             10 => 0,
+             11 => 0,
+             12 => 0}
+count = 0
+die.each do |num|
+  die.each do |num1|
+    count += 1
+    sum = num + num1
+    sum_total[sum] = sum_total[sum] +1
+
+end
+end
+
+sum_total.each do |key,value|
+  puts "The odds of #{key} coming up are #{value*2.7.round}%"
+end
